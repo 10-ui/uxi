c3887ca0141498a98422e1a2521b526b8d12cd35
10-ui <ui-10@outlook.jp>
2024-03-10 23:07:08 +0900

    フォントの追加と修正

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,3 +1,29 @@
-import { Inter } from "next/font/google";
+import { Inter } from 'next/font/google';
+import local from 'next/font/local';
 
-export const inter = Inter({ subsets: ["latin"] });
+const LINE = local({
+  src: [
+    {
+      path: '/fonts/LINESeedJP_OTF_Th.woff2',
+      weight: '100',
+      style: 'normal',
+    },
+    {
+      path: '/fonts/LINESeedJP_OTF_Rg.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '/fonts/LINESeedJP_OTF_Bd.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '/fonts/LINESeedJP_OTF_Xb.woff2',
+      weight: '800',
+      style: 'normal',
+    },
+  ],
+});
+
+export const inter = Inter({ subsets: ['latin'] });
